8ec19b77d3a3360ff669b2347c1a3ff5f4bf552f
zzRulz <nn.rulz@gmail.com>
2025-08-10 21:35:12 +0200

    Suppression des fichiers HTML

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB-gDB8UF79FNHkHhlLavrzvjeOb6vtRh8",
-  authDomain: "conquest-db-c9106.firebaseapp.com",
-  databaseURL: "https://conquest-db-c9106-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "conquest-db-c9106",
-  storageBucket: "conquest-db-c9106.firebasestorage.app",
-  messagingSenderId: "190573879824",
-  appId: "1:190573879824:web:3ee02b7f14214d877ee095"
-};
